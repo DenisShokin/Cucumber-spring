5a30e0ac5c6699a55fc4063247edd47758d2fb79
U_M1BKT <DShokin@alfabank.ru>
2022-12-02 11:18:22 +0300

    fix api test

--- src/test/java/aft/steps/SearchCucumberStepDefinitions.java
+++ src/test/java/aft/steps/SearchCucumberStepDefinitions.java
@@ -1,17 +1,38 @@
 package aft.steps;
 
+import io.cucumber.java.Before;
 import io.cucumber.java.ru.Когда;
 import io.restassured.RestAssured;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.filter.log.LogDetail;
+import io.restassured.http.ContentType;
+import io.restassured.specification.RequestSpecification;
 
 public class SearchCucumberStepDefinitions {
+
+    public final String BASE_URL = "https://yandex.ru";
+    public RequestSpecification requestSpec;
+
+    @Before
+    public void setUp() {
+        requestSpec = new RequestSpecBuilder()
+                .setBaseUri(BASE_URL)
+                .setBasePath("/search")
+                .setAccept(ContentType.JSON)
+                .log(LogDetail.ALL)
+                .build();
+    }
 
     @Когда("пользователь выполнил успешный поиск запроса: {string}")
     public void searchQuery(String query) {
         RestAssured
                 .given()
-                .get("https://yandex.ru/search/?text=" + query)
+                .spec(requestSpec)
+                    .queryParam("text",query)
+                .when()
+                .get()
                 .then()
-                .assertThat().statusCode(200)
+                .statusCode(200)
                 .log().all();
     }
 
