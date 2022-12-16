9f7567964f1bf1f2434f9402a74a56153023f26b
U_M1BKT <DShokin@alfabank.ru>
2022-12-16 14:54:19 +0300

    create testContext

---
+++ src/main/java/aft/core/context/TestContext.java
@@ -0,0 +1,25 @@
+package aft.core.context;
+
+import java.util.HashMap;
+import java.util.Map;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.stereotype.Component;
+
+@Component
+public class TestContext {
+
+    @Autowired
+    public static ApplicationContext applicationContext;
+
+    private Map<String, Object> map = new HashMap<>();
+
+    public void addVar(String name, Object var) {
+        map.put(name, var);
+    }
+
+    public <T> T getVar(String name, Class<T> classType) {
+        return classType.cast(map.get(name));
+    }
+
+}
